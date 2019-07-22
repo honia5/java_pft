370c2309cf34acef44af646158e3d4e8ef1dbfd2
Robert <robert.honca@gmail.com>
2019-07-22 23:24:09 +0200

    Added some function

--- sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqu/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,37 @@
 package ru.stqu.pft.sandbox;
 
+import javax.sound.midi.Soundbank;
+
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+  public static void main(String[] args) {
 
+    hello("world");
+    hello("text");
+    hello("robert");
+
+    double len = 5;
+
+    System.out.println("Powieszchnia o boku " + len + "równa sie " + area(len));
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Powieszchnia prostokątu o boku " + a + " i " + b + "  równa sie " + area(a, b));
+  }
+
+  public static void hello(String sombody) {
+    System.out.println("Hello " + sombody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
+
